b9c4c390f33bace6f8a9de940fca6e71828414d5
Nick Bevers <nick.bevers9@gmail.com>
2023-07-30 21:56:56 +0200

    feat: add parent page to contain all configurator related content

--- src/pages/ConfiguratorPage.vue
+++ src/pages/ConfiguratorPage.vue
@@ -1,24 +1,58 @@
 <script setup lang="ts">
     import ConfigIndicator from '../components/ConfigIndicator.vue';
+    import ConfiguratorSideBar from '../components/ConfiguratorSideBar.vue';
+    import Configurator from '../components/Configurator.vue';
+    import { ref, Ref } from 'vue';
 
-    let currentVisual = 2;
+    const currentPhase: Ref<number> = ref(1);
+
+    // create an interface for the visual event to get the textContent of the clicked circle
     interface VisualChangeEvent extends Event {
         detail: number
     }
 
-    const stepChange = (event: Event) => {
-        currentVisual = (event as VisualChangeEvent).detail;
+    // data to be stored in the database
+    const data = {
+        flavour: '',
+        cupColor: '',
+        logoUrl: '',
+        logoPID: '',
+        toppings: [],
     }
 
+    const stepChange = (event: VisualChangeEvent) => {
+        currentPhase.value = event.detail;
+    }
+
+    const changeFlavour = (flavour: string) => {
+        data.flavour = flavour;
+        currentPhase.value = 2;
+    }
+
+    // chocolate sauce: 4D1F0C
+    // strawberry sauce: B71C27
 </script>
 
 <template>
     <div class="configurator__container">
-        <ConfigIndicator :current-visual="currentVisual" @update:current-visual="stepChange"/>
+        <ConfigIndicator :current-phase="currentPhase" @update:current-phase="stepChange"/>
+
+        <!-- Left side panel with options for the user width 33% -->
+        <ConfiguratorSideBar :current-phase="currentPhase" @set-flavour="changeFlavour"/>
+
+        <!-- ThreeJS part that wont change except for colors width 66% -->
+        <Configurator />
         
     </div>
 </template>
 
 <style scoped>
-    
+    .configurator__container{
+        display: flex;
+        flex-direction: row;
+        align-items: end;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
 </style>
